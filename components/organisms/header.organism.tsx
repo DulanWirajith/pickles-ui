4a936a13572c97a903fe59cf294ad38750f5e57d
dulanwirajith <dulanwirajith1995@gmail.com>
2023-04-08 19:27:29 +0530

    feat(template design): template design doing

---
+++ components/organisms/header.organism.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return (
+        <header className="flex justify-between px-4 pt-4">
+            <h1 className="text-4xl font-bold text-center">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
+                    Dashboard
+                </span>
+            </h1>
+        </header>
+    )
+}
