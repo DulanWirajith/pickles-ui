2446fe42e82db356c220948af8d520eb07467798
dulanwirajith <dulanwirajith1995@gmail.com>
2023-04-09 09:13:19 +0530

    feat(redux integrate): redux integration done

---
+++ store/email/email.slice.ts
@@ -0,0 +1,27 @@
+import type {PayloadAction} from '@reduxjs/toolkit'
+import {createSlice} from '@reduxjs/toolkit'
+import moment from 'moment';
+
+// Define a type for the slice state
+interface EmailState {
+    lastUpdatedDate: string
+}
+
+// Define the initial state using that type
+const initialState: EmailState = {
+    lastUpdatedDate: moment().toISOString(),
+}
+
+export const emailSlice = createSlice({
+    name: 'emails',
+    initialState,
+    reducers: {
+        setLastUpdatedDate: (state, action: PayloadAction<string>) => {
+            state.lastUpdatedDate = action.payload
+        },
+    },
+})
+
+export const {setLastUpdatedDate} = emailSlice.actions
+// export const selectCount = (state: RootState) => state.emails.lastUpdatedDate
+export const emailSliceReducer = emailSlice.reducer
