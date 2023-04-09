886123a77caa7a0f4cdca8757d2e8035da729acc
dulanwirajith <dulanwirajith1995@gmail.com>
2023-04-09 10:16:59 +0530

    refactor(): refactoring using GridContent component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import {GridContent} from "@/components/templates/grid-content.template";
 import React from "react";
+import {useAppSelector} from "@/store/hooks";
 
 export default function Home() {
+    const lastUpdatedDate = useAppSelector((state) => state.emails.lastUpdatedDate)
+
     return (
         <>
-            <GridContent headerName="Dashboard">
+            <GridContent headerName="Dashboard" lastUpdatedDate={lastUpdatedDate}>
             </GridContent>
         </>
     )
