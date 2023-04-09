7531550e32a1ee1b923fe3250c305e1c40aa19e1
dulanwirajith <dulanwirajith1995@gmail.com>
2023-04-09 12:19:42 +0530

    feat(pusher integrate): try with presense channel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    env: {
+        PUSHER_APP_ID: "1581427",
+        PUSHER_APP_KEY: "61bef8916b91f4cb5473",
+        PUSHER_APP_SECRET: "a8c2461af8abebb9af87",
+        PUSHER_APP_CLUSTER: "ap2"
+    }
 }
 
 module.exports = nextConfig
--- pages/email/index.tsx
+++ pages/email/index.tsx
@@ -7,6 +7,7 @@ import {useAppDispatch, useAppSelector} from "@/store/hooks";
 import {setLastUpdatedDate} from "@/store/email/email.slice";
 import moment from "moment/moment";
 import {GridContent} from "@/components/templates/grid-content.template";
+import Pusher from "pusher-js";
 
 export default function Email() {
     const [isPaginationChanged, setIsPaginationChanged] = useState(false);
@@ -29,7 +30,55 @@ export default function Email() {
     };
 
     const lastUpdatedDate = useAppSelector((state) => state.emails.lastUpdatedDate)
-    const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch();
+
+    // for pusher >>>>>>>>>>>>>>>
+
+    const userId = "dulan";
+    const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImUuYW5hZG9sQGdtYWlsLmNvbSIsImlhdCI6MTY2NjYwNzM4MiwiZXhwIjoxNjY3OTAzMzgyfQ.IMz3g1Lj4oC5Bw64QnyWjRc_hJZMgqSJ7VcukkaCkFAo"
+
+    useEffect(() => {
+        console.log('PUSHER_APP_ID >>', process.env.PUSHER_APP_ID);
+        console.log('REACT_APP_CLUSTER >>', process.env.PUSHER_APP_CLUSTER);
+        const pusher = new Pusher(`${process.env.PUSHER_APP_ID}`, {
+            cluster: `${process.env.PUSHER_APP_CLUSTER}`,
+            authEndpoint: "http://localhost:3000/mail/auth",
+            auth: {
+                params: {
+                    userId,
+                },
+            },
+        });
+
+        console.log('pusher >>', pusher)
+
+        const channel = pusher.subscribe(`presence-${userId}`);
+        console.log('channel>>', channel)
+
+        try {
+            channel.bind("pusher:subscription_succeeded", (members: any) => {
+                // getNotifications();
+                console.log("subscription_succeeded", members);
+                // setIsPaginationChanged(!isPaginationChanged);
+            });
+        } catch (e) {
+            console.log(e)
+        }
+
+        console.log('asd')
+
+        channel.bind("new_notification", (data: any) => {
+            console.log(data);
+        });
+
+        return () => {
+            pusher.unsubscribe(`presence-${userId}`);
+        };
+    }, []);
+
+
+    // for pusher <<<<<<<<<<<<<<<<
 
     const columns = [
         {
